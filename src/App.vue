49f598c2a8340774548b108a551747447367fd40
sam921611 <sam921611@kimo.com>
2021-10-21 10:42:25 +0800

    基本樣式

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
-  <div id="app">
-    
-  </div>
+	<div id="app">
+		<div class="container-app">
+			<div class="container-quiz">
+				<div class="quiz-header">
+					<h1>Quiz app</h1>
+				</div>
+
+				<div class="quiz-main">
+					<h1>main</h1>
+				</div>
+
+				<div class="quiz-footer">
+					<h1>footer</h1>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+	name: 'App',
+	components: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	
 }
 </style>
